febfe004d629907b0708b0882118bdf77c3bf636
Mason Chen <b08901058@ntu.edu.tw>
2021-12-28 23:55:34 +0800

    can track position with gps and setOrigin with GPS coord

---
+++ README.md
@@ -0,0 +1,7 @@
+# Magic Bike
+
+how to run
+
+```
+$ python3 mapbox.py
+```
--- mapbox.py
+++ mapbox.py
@@ -1,6 +1,5 @@
-import requests
 from flask import Flask, render_template, request, jsonify
-import json
+from GPS import get_coord
 
 app = Flask(__name__)
 
@@ -8,21 +7,21 @@ app = Flask(__name__)
 def home():
     return render_template("navigation.html")
 
-@app.route("/api", methods=['POST'])
-def api():
-    print(request.json)
-    return jsonify(request.json)
-# app.run()
-
-# app.run(host="0.0.0.0", port=3000)
-app.run(host="127.0.0.1", port=3000)
+@app.route("/api/navigate", methods=['POST'])
+def navigate():
+    request_data = request.json
+    return jsonify(request_data)
 
 
-# response = requests.get('https://api.mapbox.com/directions/v5/mapbox/cycling/121.542678%2C25.015552%3B121.535821%2C25.019733?alternatives=false&continue_straight=true&geometries=geojson&language=en&overview=simplified&steps=true&access_token=pk.eyJ1IjoicmVkeG91bHMiLCJhIjoiY2t4N2R1Nm1uMHl4aTJwcXViYno1Ym9sNCJ9.fByzZrach_1gQlboB02hCg')
-# query_raw = response.json()
-# routes = query_raw["routes"][0]["legs"][0]["steps"]
-# html_route = []
-# for i, route in enumerate(routes):
-#     route_str = "%s: %s" % (route["maneuver"].get("modifier"), route["maneuver"].get("instruction"))
-#     html_route.append(route_str)
-# # return json.dumps(html_route)
+@app.route("/api/current", methods=['GET'])
+def current():
+    coord = get_coord()
+    current_location = {
+        "coordinates": coord
+    }
+    return jsonify(current_location)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
+# app.run(host="127.0.0.1", port=3000)
