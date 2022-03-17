87b0a18c5cf50d9dddde5b4946442f6c6a2f38bf
Mason Chen <b08901058@ntu.edu.tw>
2022-03-17 17:14:25 +0800

    migrate to vite

--- src/Map.js
+++ src/Map.js
@@ -3,18 +3,6 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
 import "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css";
 
-import { PositionAPI } from "./api.js";
-
-import {
-  SetOriginControl,
-  PitchControl,
-  CompassControl,
-  NavigateControl,
-  CameraControl,
-} from "./controls";
-
-import LocationMarker from "./markers/Location.js";
-
 mapboxgl.accessToken =
   "pk.eyJ1IjoicmVkeG91bHMiLCJhIjoiY2t4N2R1Nm1uMHl4aTJwcXViYno1Ym9sNCJ9.fByzZrach_1gQlboB02hCg";
 
@@ -29,166 +17,15 @@ class Map {
       zoom: 16,
     });
 
-    // Creates new directions control instance
-    this.directions = new MapboxDirections({
-      accessToken: mapboxgl.accessToken,
-      unit: "metric",
-      profile: "mapbox/cycling",
-      interactive: true,
-      controls: {
-        inputs: true,
-        instructions: false,
-        profileSwitcher: true,
-      },
-    });
-
-    // Integrates directions control with map
-    this.map.addControl(this.directions, "top-left");
-
     const navigation = new mapboxgl.NavigationControl({ showCompass: false });
     this.map.addControl(navigation);
 
-    const coordinates = [121.54373533333333, 25.0190466666666684];
+    // const coordinates = [121.54373533333333, 25.0190466666666684];
 
-    this.currentMarker = new mapboxgl.Marker(LocationMarker())
-      .setLngLat(coordinates)
-      .setRotation(0)
-      .addTo(this.map);
-
-    this.map.addControl(
-      new CompassControl({
-        updateCurentMarkerBearing: this.updateCurentMarkerBearing.bind(this),
-      }),
-      "top-right"
-    );
-
-    this.map.addControl(
-      new SetOriginControl({
-        updateCallback: this.updateStartPoint.bind(this),
-      }),
-      "top-right"
-    );
-
-    this.map.addControl(
-      new NavigateControl({
-        startCallback: this.startNavigation.bind(this),
-        stopCallback: this.stopNavigation.bind(this),
-        updateMapBearing: this.updateMapBearing.bind(this),
-      }),
-      "top-right"
-    );
-
-    this.map.addControl(new PitchControl({ minpitchzoom: 17 }));
-
-    this.directions.on("origin", (e) => {
-      console.log(e.feature.geometry.coordinates);
-      fetch("/api/navigate", {
-        body: JSON.stringify({ start: e.feature.geometry.coordinates }), // must match 'Content-Type' header
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-      })
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (myJson) {
-          console.log(myJson);
-        });
-    });
-
-    this.map.addControl(new CameraControl({}));
-
-    this.directions.on("destination", (e) => {
-      console.log(e.feature.geometry.coordinates);
-      fetch("/api/navigate", {
-        body: JSON.stringify({ end: e.feature.geometry.coordinates }), // must match 'Content-Type' header
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-      })
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (myJson) {
-          console.log(myJson);
-        });
-    });
-
-    this.updateCurentMarkerPostion();
-    // this.updateCurentMarkerBearing()
-    // this.updateMapBearing();
-
-    // Add markers to the map.
-  }
-
-  updateCurentMarkerPosition() {
-    console.log("update my position");
-    const { currentMarker } = this;
-    setInterval(
-      () => {
-        PositionAPI.getCurrentPosition().then((response) => {
-          console.log(response);
-          if (response) {
-            const { coordinates } = response.data;
-            currentMarker.setLngLat(coordinates);
-          }
-        });
-      },
-      1000,
-      currentMarker
-    );
-  }
-
-  updateCurentMarkerBearing() {
-    PositionAPI.getCurentBearing().then((response) => {
-      console.log(response);
-      if (response) {
-        const { bearing } = response.data;
-        this.currentMarker.setRotation(bearing);
-      }
-    });
-  }
-
-  updateMapBearing() {
-    PositionAPI.getCurentBearing().then((response) => {
-      if (response) {
-        const { bearing } = response.data;
-        this.map.easeTo({ pitch: 0, bearing });
-      }
-    });
-  }
-
-  updateStartPoint() {
-    PositionAPI.getCurentPosition().then(
-      function (response) {
-        if (response) {
-          const { coordinates } = response.data;
-          this.directions.setOrigin(coordinates);
-        }
-      }.bind(this)
-    );
-  }
-
-  updateBearing() {
-    PositionAPI.getCurentBearing().then(
-      function (response) {
-        if (response) {
-          const { bearing } = response.data;
-          this.map.easeTo({ pitch: 0, bearing });
-          console.log(bearing);
-        }
-      }.bind(this)
-    );
-  }
-
-  startNavigation() {
-    PositionAPI.startNavigation();
-  }
-
-  stopNavigation() {
-    PositionAPI.stopNavigation();
+    // this.currentMarker = new mapboxgl.Marker()
+    //   .setLngLat(coordinates)
+    //   .setRotation(0)
+    //   .addTo(this.map);
   }
 }
 
--- src/controls/index.js
+++
@@ -1,7 +0,0 @@
-import "./Controls.css";
-
-export { default as SetOriginControl } from "./setOriginControl.js";
-export { default as PitchControl } from "./pitchControl.js";
-export { default as CompassControl } from "./compassControl.js";
-export { default as NavigateControl } from "./navigateControl.js";
-export { default as CameraControl } from "./cameraControl.js"
